29db225c9a90f4a0ddfaa4aa0912b5c632d64e8b
Gurkan Tongel <gurkantngl@gmail.com>
2023-12-09 16:51:09 +0300

    delete google cloud info

--- fitlife/settings.py
+++ fitlife/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fitlife.yazlab@gmail.com'
-EMAIL_HOST_PASSWORD = 'knci okod vyfu htzx'
+EMAIL_HOST_USER = 'your_email'
+EMAIL_HOST_PASSWORD = 'your_password'
